e38489e98dd9ce95706a44ea93678ec930cd66b3
MarcoAntonioPdLA <mponcedeleona@unsa.edu.pe>
2026-05-04 02:33:09 -0500

    Agregando detección de cajas en objetivo.

--- Assets/Scripts/ObjectiveCollisionChecker.cs
+++ Assets/Scripts/ObjectiveCollisionChecker.cs
@@ -1,5 +1,17 @@
 using UnityEngine;
 
 public class ObjectiveCollisionChecker : MonoBehaviour {
-    
+    public ObjectivesManager objectivesManager;
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.CompareTag("Box")) {
+            objectivesManager.AddPoint();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider) {
+        if (collider.CompareTag("Box")) {
+            objectivesManager.RemovePoint();
+        }
+    }
 }
--- Assets/Scripts/ObjectivesManager.cs
+++ Assets/Scripts/ObjectivesManager.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
 
-public class ObjectivesManager : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+public class ObjectivesManager : MonoBehaviour {
+    [SerializeField] private int pointsObjective = 4;
+    private int points = 0;
+
+    public void AddPoint() {
+        points++;
+        Debug.Log(points);
+        if(points >= pointsObjective) {
+            Debug.Log("Ganaste");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void RemovePoint() {
+        points = Mathf.Max(points - 1, 0);
+        Debug.Log(points);
     }
 }
